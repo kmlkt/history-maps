5396045a2bf7c98eadc5c35e53b047b374cfa84e
karim <450906k@gmail.com>
2022-08-17 17:48:06 +0500

    start migrating to typescript & new file format

---
+++ gulpfile.mjs
@@ -0,0 +1,33 @@
+import gulp from "gulp";
+import browserify from "browserify";
+import source from "vinyl-source-stream";
+import tsify from "tsify";
+
+var paths = {
+  pages: ["src/*.html"],
+};
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+
+gulp.task('copy-css', function() {
+  return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task(
+  "default",
+  gulp.series(gulp.parallel("copy-html", "copy-css"), function () {
+    return browserify({
+      basedir: ".",
+      debug: true,
+      entries: ["src/ts/main.ts"],
+      cache: {},
+      packageCache: {},
+    })
+      .plugin(tsify)
+      .bundle()
+      .pipe(source("bundle.js"))
+      .pipe(gulp.dest("dist"));
+  })
+);
---
+++ src/ts/main.ts
@@ -0,0 +1,3 @@
+import controls from './gui/controls';
+controls.eventName.textContent = 'hello';
+console.log('hello world');
